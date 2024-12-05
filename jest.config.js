1c7e5e7a00da4ea92675cb1f2fb938d5175e7df0
iarla-crewe <iarla@quartzpay.io>
2024-12-05 17:13:47 +0000

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    extensionsToTreatAsEsm: [".ts"],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    }
 };
