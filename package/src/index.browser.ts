2531b2fc5fee5e91f767901bda71bbf0f3d0939d
iarla-crewe <iarla@quartzpay.io>
2024-12-22 23:56:45 +0000

    Add browser entrypoint

--- package/src/index.browser.ts
+++ package/src/index.browser.ts
@@ -1,6 +1,8 @@
 export type { BN } from "@coral-xyz/anchor";
-export { Wallet } from "@coral-xyz/anchor";
 export { QuartzClient } from "./client.js";
 export { QuartzUser } from "./user.js";
 export * from "./config/constants.js";
 export * from "./utils/helpers.js";
+
+import type { Wallet } from "@coral-xyz/anchor/dist/cjs/provider.js";
+export type { Wallet };
